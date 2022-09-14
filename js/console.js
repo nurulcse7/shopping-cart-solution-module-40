97e624c80292daeee7f50b7636a23434e38f1a4e
nurulcse7 <nurul.cse7@gmail.com>
2022-09-14 09:08:31 +0600

    added console.js and error.js

---
+++ js/console.js
@@ -0,0 +1,25 @@
+
+console.assert
+console.clear
+console.count
+console.countReset
+console.debug
+console.dir
+console.dirxml
+console.error
+console.group
+console.groupCollapsed
+console.groupEnd
+console.info
+console.log  // used for output
+console.table
+console.time
+console.timeEnd
+console.timeLog
+console.timeStamp
+console.trace
+console.warn // warning // console.warn('If you come here, I will beat you')
+
+// details: 
+// https://developer.chrome.com/docs/devtools/console/
+// https://www.geeksforgeeks.org/console-in-javascript/
---
+++ js/error.js
@@ -0,0 +1,11 @@
+// 01. syntax error
+
+// console.log(5)
+// console.log(5;
+
+// 02. Type error
+let address;
+console.log(typeof address)
+console.log(address.length)
+
+
